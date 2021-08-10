6c6528cd8572d06b47a097bc33d4517139f239a0
AlanaZUP <alana.reis@zup.com.br>
2021-08-10 18:19:26 -0300

    2 - Enviando Parâmetros

---
+++ Trabalhando com POST e GET/gerenciador/src/main/java/NovaEmpresa.java
@@ -0,0 +1,28 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/novaEmpresa")
+public class NovaEmpresa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		System.out.println("Cadastrando nome: " + nome);
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("A Empresa " + nome + " foi cadastrada!");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
